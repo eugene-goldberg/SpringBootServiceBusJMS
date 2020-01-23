7fe4a6d1fd1ef15b55746040d6d18f3f82b8be84
aceteam.dfs@gmail.com <eugene@Eugenes-MacBook-Pro-2.local>
2020-01-23 15:10:12 -0600

    initial commit with working azure SP and JMS - send and recieve to topics

---
+++ src/main/java/com/example/demo/QueueReceiveController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueueReceiveController {
+
+    private static final String QUEUE_NAME = "sbqueue0001968";
+
+    private final Logger logger = LoggerFactory.getLogger(QueueReceiveController.class);
+
+    @JmsListener(destination = QUEUE_NAME, containerFactory = "jmsListenerContainerFactory")
+    public void receiveMessage(User user) {
+        logger.info("Received message from queue: {}", user.getName());
+    }
+}
---
+++ src/main/java/com/example/demo/TopicReceiveController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicReceiveController {
+
+    private static final String TOPIC_NAME = "servicebustopic0001968";
+
+    private static final String SUBSCRIPTION_NAME = "sbtopicsubscription0001968";
+
+    private final Logger logger = LoggerFactory.getLogger(TopicReceiveController.class);
+
+    @JmsListener(destination = TOPIC_NAME, containerFactory = "topicJmsListenerContainerFactory",
+            subscription = SUBSCRIPTION_NAME)
+    public void receiveMessage(User user) {
+        logger.info("Received message from topic: {}", user.getName());
+    }
+}
